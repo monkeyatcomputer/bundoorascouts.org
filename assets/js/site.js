264cdbbfdc9efee95faabfb9c0d489409e5100de
monkeyatcomputer <monkeyatcomputer@users.noreply.github.com>
2023-11-12 04:28:42 +0000

    published by GitHub Actions

---
+++ assets/js/site.js
@@ -0,0 +1,56 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Preloader (if the #preloader div exists)
+  $(window).on('load', function () {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function () {
+        $(this).remove();
+      });
+    }
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  $('.parallax-background').jarallax({
+    speed: 0.5,
+    imgWidth: 1366,
+    imgHeight: 768
+    });
+  
+  if ($.fn.simpleLightbox) {
+    $('.image-gallery a').simpleLightbox();
+  }
+  
+  // Disable Google Maps scrolling
+  // See http://stackoverflow.com/a/25904582/1607849
+  // Disable scroll zooming and bind back the click event
+  var onMapMouseleaveHandler = function(event) {
+    var that = $(this);
+    that.on('click', onMapClickHandler);
+    that.off('mouseleave', onMapMouseleaveHandler);
+    that.find('iframe').css("pointer-events", "none");
+  }
+  var onMapClickHandler = function(event) {
+    var that = $(this);
+    // Disable the click handler until the user leaves the map area
+    that.off('click', onMapClickHandler);
+    // Enable scrolling zoom
+    that.find('iframe').css("pointer-events", "auto");
+    // Handle the mouse leave event
+    that.on('mouseleave', onMapMouseleaveHandler);
+  }
+  // Enable map zooming with mouse scroll when the user clicks the map
+  $('.map').on('click', onMapClickHandler);
+})(jQuery); // End of use strict
